0d6cfc4339bf2b95eb384ae3bdee3cf239f4aa59
Nimish Wadhwa <nimish.wadhwa@akqa.com>
2023-08-14 13:49:11 +0530

    sidebar structure

---
+++ src/app/containers/Header/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from './header.module.scss'
+
+const Header = () => {
+  return (
+    <header className={styles.header}></header>
+  );
+};
+export default Header;
